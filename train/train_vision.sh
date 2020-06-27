a5523676157f5c183dddf7b86fa34f9d8028745a
Qu Shen <503578404@qq.com>
2020-06-27 18:24:30 +0800

    Add training for yolov5.

--- train/train_vision.sh
+++ train/train_vision.sh
@@ -2,12 +2,12 @@
 cd ../yolov5 || exit
 
 # DJI
-# python3 train.py --data ../train/roco_dataset.yaml --cache-images --img-size 416 416 --cfg models/roco_model.yaml
+python3 train.py --data ../train/roco_dataset.yaml --img-size 853 480 --cfg ../train/roco_model.yaml
 
 # BCCD
 # python3 train.py --data ../BCCD/data.yaml --cache-images --img-size 416 416 --cfg ../BCCD/model.yaml
 
 # COCO
-python3 train.py --data data/coco.yaml
+# python3 train.py --data data/coco.yaml
 
 cd ../train || exit
