861f40c9f5ee4d4e40995121cdd9bf55ea620a5b
Qu Shen <503578404@qq.com>
2020-08-11 13:14:34 +0800

    Add more file

---
+++ src/app/auto_aim/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    std::cout << "Auto aimer started." << std::endl;
+    return 0;
+}
---
+++ src/camera/camera.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+class Camera
+{
+private:
+    /* data */
+public:
+    Camera(/* args */);
+    ~Camera();
+};
+
+Camera::Camera(/* args */)
+{
+}
+
+Camera::~Camera()
+{
+}
