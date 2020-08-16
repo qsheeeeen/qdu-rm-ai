359a616f50f2e27ff23a76d8069eeff1b1d66f5e
Qu Shen <503578404@qq.com>
2020-08-16 15:13:45 +0800

    Add camera class

--- src/camera/camera.cpp
+++ src/camera/camera.cpp
@@ -6,12 +6,65 @@
 
 using namespace cv;
 
-Camera::Camera(/* args */)
+void Camera::LoadTargetHist()
+{
+    frame_in = imread("./image/hist_target.jpg");
+    calcHist(&frame_in, 1, hist_ch, Mat(), hist_target, 3, hist_size, hist_range);
+}
+
+void Camera::MatchTargetHist()
+{
+    calcHist(&frame_in, 1, hist_ch, Mat(), hist_in, 3, hist_size, hist_range);
+    double min, max;
+    minMaxIdx(hist_in, &min, &max);
+}
+
+void Camera::CreateLUT()
+{
+    lut.create(1, 256, CV_8U);
+
+    for (int i = 0; i < 256; i++)
+        lut.at<float>(i) = saturate_cast<uint8_t>(pow(i / 255.0, gamma) * 255.0);
+}
+
+void Camera::AppyLUT()
+{
+    LUT(frame_in, lut, frame_in);
+}
+
+Camera::Camera(int index)
 {
     std::cout << "Create Camera." << std::endl;
+
+    this->CreateLUT();
+    this->LoadTargetHist();
+
+    cam.open(index, CAP_V4L2);
 }
 
 Camera::~Camera()
 {
-    std::cout << "Destroy Camera." << std::endl;
+    cam.release();
+    std::cout << "Camera Destried." << std::endl;
+}
+
+bool Camera::Capture()
+{
+    cam >> frame_in;
+    return frame_in.empty();
+}
+
+void Camera::Preprocess()
+{
+    this->AppyLUT();
+    this->MatchTargetHist();
+}
+
+void Camera::GetFrame(cv::Mat &output)
+{
+    frame_in.copyTo(output);
+}
+
+void Camera::CalcTargetHist()
+{
 }
