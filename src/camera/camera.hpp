0440bc48fe4c6087482d9a84e6aa985005a6ed61
Qu Shen <503578404@qq.com>
2020-08-16 15:13:45 +0800

    Add camera class

--- src/camera/camera.hpp
+++ src/camera/camera.hpp
@@ -5,11 +5,32 @@
 class Camera
 {
 private:
-    cv::VideoCapture cap;
+    cv::VideoCapture cam;
+    cv::Mat frame_in;
+    cv::Mat hist_in;
+    cv::Mat hist_target;
+
+    const int hist_ch[3] = {0, 1, 2};
+    const int hist_size[3] = {256, 256, 256};
+    float hist_ch_range[2] = {0., 255.};
+    const float *hist_range[3] = {hist_ch_range, hist_ch_range, hist_ch_range};
+
+    float gamma = 1.;
+    cv::Mat lut;
+
+    void CreateLUT();
+    void AppyLUT();
+
+    void LoadTargetHist();
+
+    void GetFrame(cv::Mat &output);
+
+    void MatchTargetHist();
 
 public:
-    Camera(/* args */);
+    Camera(int index);
     ~Camera();
-    void Capture();
+    bool Capture();
     void Preprocess();
+    void CalcTargetHist();
 };
