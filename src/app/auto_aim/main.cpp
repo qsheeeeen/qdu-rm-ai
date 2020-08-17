18809e2bd826336cd62e29cede8d270f472765fc
Qu Shen <503578404@qq.com>
2020-08-17 21:30:54 +0800

    Add thread test.

--- src/app/auto_aim/main.cpp
+++ src/app/auto_aim/main.cpp
@@ -1,20 +1,36 @@
 #include <iostream>
+#include <thread>
 
 #include "behavior.hpp"
 #include "camera.hpp"
+#include "robot.hpp"
 #include "vision.hpp"
 
-int main(int argc, char const *argv[]) {
+void processing_robot(Robot& robot) {
+  for (int i = 0; i < 5; ++i) {
+    std::cout << "Robot thread executing.\n" << i << std::endl;
+    // robot.Parse();
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+  }
+}
+
+int main(int argc, char const* argv[]) {
   (void)argc;
   (void)argv;
+
   std::cout << "Auto aimer started." << std::endl;
 
+  Robot bot;
+  std::thread robot_thread(processing_robot, std::ref(bot));
   // Test only. TOOD: Remove.
-  auto cam = Camera(0);
+
+  // Camera cam(0);
 
   TestTree();
   TestOpenCV();
   TestVideoWrite();
+
+  robot_thread.join();
 
   // Init behavior.
   // Run true tree.
