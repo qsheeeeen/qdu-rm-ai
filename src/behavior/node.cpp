b5cd91bfd5767b6e41bb419f7d00b012054babd1
Qu Shen <503578404@qq.com>
2020-08-17 21:27:58 +0800

    Move all definition to cpp file

--- src/behavior/node.cpp
+++ src/behavior/node.cpp
@@ -2,14 +2,26 @@
 
 namespace Action {
 
+Track::Track(const std::string& name) : BT::SyncActionNode(name, {}) {
+  std::cout << "Creat Action::Track node: " << this->name() << std::endl;
+}
+
 BT::NodeStatus Track::tick() {
   std::cout << this->name() << ": Tracking." << std::endl;
   return BT::NodeStatus::SUCCESS;
 }
 
+Attack::Attack(const std::string& name) : BT::SyncActionNode(name, {}) {
+  std::cout << "Creat Action::Attack node: " << this->name() << std::endl;
+}
+
 BT::NodeStatus Attack::tick() {
   std::cout << this->name() << ": Attacking." << std::endl;
   return BT::NodeStatus::SUCCESS;
+}
+
+Dodge::Dodge(const std::string& name) : BT::SyncActionNode(name, {}) {
+  std::cout << "Creat Action::Dodge node: " << this->name() << std::endl;
 }
 
 BT::NodeStatus Dodge::tick() {
@@ -21,9 +33,19 @@ BT::NodeStatus Dodge::tick() {
 
 namespace Condition {
 
+EnamyVisable::EnamyVisable(const std::string& name)
+    : BT::ConditionNode(name, {}) {
+  std::cout << "Creat Condition::EnamyVisable node: " << this->name()
+            << std::endl;
+}
+
 BT::NodeStatus EnamyVisable::tick() {
   std::cout << this->name() << ": Check EnamyVisable." << std::endl;
   return BT::NodeStatus::SUCCESS;
+}
+
+LowHP::LowHP(const std::string& name) : BT::ConditionNode(name, {}) {
+  std::cout << "Creat Condition::LowHP node: " << this->name() << std::endl;
 }
 
 BT::NodeStatus LowHP::tick() {
@@ -31,9 +53,19 @@ BT::NodeStatus LowHP::tick() {
   return BT::NodeStatus::FAILURE;
 }
 
+UnderAttack::UnderAttack(const std::string& name)
+    : BT::ConditionNode(name, {}) {
+  std::cout << "Creat Condition::UnderAttack node: " << this->name()
+            << std::endl;
+}
+
 BT::NodeStatus UnderAttack::tick() {
   std::cout << this->name() << ": Check UnderAttack." << std::endl;
   return BT::NodeStatus::SUCCESS;
+}
+
+NoAmmo::NoAmmo(const std::string& name) : BT::ConditionNode(name, {}) {
+  std::cout << "Creat Condition::NoAmmo node: " << this->name() << std::endl;
 }
 
 BT::NodeStatus NoAmmo::tick() {
--- src/behavior/node.hpp
+++ src/behavior/node.hpp
@@ -4,71 +4,50 @@
 #include "behaviortree_cpp_v3/bt_factory.h"
 
 namespace Action {
+
 class Track : public BT::SyncActionNode {
  public:
-  Track(const std::string &name) : BT::SyncActionNode(name, {}) {
-    std::cout << "Creat Action::Track node: " << this->name() << std::endl;
-  }
-
+  Track(const std::string &name);
   BT::NodeStatus tick() override;
 };
 
 class Attack : public BT::SyncActionNode {
  public:
-  Attack(const std::string &name) : BT::SyncActionNode(name, {}) {
-    std::cout << "Creat Action::Attack node: " << this->name() << std::endl;
-  }
-
+  Attack(const std::string &name);
   BT::NodeStatus tick() override;
 };
 
 class Dodge : public BT::SyncActionNode {
  public:
-  Dodge(const std::string &name) : BT::SyncActionNode(name, {}) {
-    std::cout << "Creat Action::Dodge node: " << this->name() << std::endl;
-  }
-
+  Dodge(const std::string &name);
   BT::NodeStatus tick() override;
 };
 
 }  // namespace Action
 
 namespace Condition {
+
 class EnamyVisable : public BT::ConditionNode {
  public:
-  EnamyVisable(const std::string &name) : BT::ConditionNode(name, {}) {
-    std::cout << "Creat Condition::EnamyVisable node: " << this->name()
-              << std::endl;
-  }
-
+  EnamyVisable(const std::string &name);
   BT::NodeStatus tick() override;
 };
 
 class LowHP : public BT::ConditionNode {
  public:
-  LowHP(const std::string &name) : BT::ConditionNode(name, {}) {
-    std::cout << "Creat Condition::LowHP node: " << this->name() << std::endl;
-  }
-
+  LowHP(const std::string &name);
   BT::NodeStatus tick() override;
 };
 
 class UnderAttack : public BT::ConditionNode {
  public:
-  UnderAttack(const std::string &name) : BT::ConditionNode(name, {}) {
-    std::cout << "Creat Condition::UnderAttack node: " << this->name()
-              << std::endl;
-  }
-
+  UnderAttack(const std::string &name);
   BT::NodeStatus tick() override;
 };
 
 class NoAmmo : public BT::ConditionNode {
  public:
-  NoAmmo(const std::string &name) : BT::ConditionNode(name, {}) {
-    std::cout << "Creat Condition::NoAmmo node: " << this->name() << std::endl;
-  }
-
+  NoAmmo(const std::string &name);
   BT::NodeStatus tick() override;
 };
 }  // namespace Condition
