4a7a0a9759cb66bfbe5becf18a6cf9f1e5340acc
Qu Shen <503578404@qq.com>
2020-08-22 17:19:12 +0800

    Add work thread to robot.

--- src/robot/robot.cpp
+++ src/robot/robot.cpp
@@ -1,23 +1,30 @@
 #include "robot.hpp"
 
-#include <fstream>
 #include <iostream>
 
 void Robot::WorkThread() {
   while (continue_parse_) {
-    dev_.read(buff, sizeof(holder_t));
+    dev_.read(recv_buff_, sizeof(recv_holder_t));
     Parse();
+    commandq_mutex.lock();
+    if (!commandq_.empty()) {
+      dev_.write((char*)&commandq_.front(), sizeof(recv_holder_t));
+      commandq_.pop();
+    }
+    commandq_mutex.unlock();
     std::this_thread::sleep_for(std::chrono::milliseconds(10));
   }
 }
+
+void ParseThread();
 
 void Robot::Parse() {}
 
 Robot::Robot(const std::string &dev_path) {
   std::cout << "Create Robot." << std::endl;
 
-  dev_.open(dev_path);
-  if (!dev_.is_open()) throw std::runtime_error("Can't open device.");
+  dev_.open(dev_path, std::ios::binary);
+  if (!dev_.is_open()) throw std::runtime_error("Can't open Robot device.");
 
   continue_parse_ = true;
   parse_thread_ = std::thread(&Robot::WorkThread, this);
--- src/robot/robot.hpp
+++ src/robot/robot.hpp
@@ -1,19 +1,27 @@
 #pragma once
 
 #include <fstream>
+#include <mutex>
+#include <queue>
 #include <thread>
 
 typedef struct {
   float holder;
-} holder_t;
+} recv_holder_t;
+
+typedef struct {
+  float holder;
+} command_holder_t;
 
 class Robot {
  private:
   std::fstream dev_;
   bool continue_parse_ = false;
   std::thread parse_thread_;
+  std::queue<command_holder_t> commandq_;
+  std::mutex commandq_mutex;
 
-  char buff[sizeof(holder_t)];
+  char recv_buff_[sizeof(recv_holder_t)];
   void WorkThread();
   void Parse();
 
