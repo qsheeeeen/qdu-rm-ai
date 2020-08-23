c30ff46a56c4882bb31dc7fe03ab2455427fcef8
Qu Shen <503578404@qq.com>
2020-08-23 14:11:09 +0800

    Add logging.

--- src/robot/robot.cpp
+++ src/robot/robot.cpp
@@ -2,33 +2,49 @@
 
 #include <iostream>
 
+#include "spdlog/spdlog.h"
+
 void Robot::ComThread() {
+  spdlog::debug("[Robot][ComThread] Running.");
+
   while (continue_parse_) {
-    dev_.read((char*)&status_, sizeof(recv_holder_t));
+    dev_.read((char *)&status_, sizeof(recv_holder_t));
 
     commandq_mutex_.lock();
     if (!commandq_.empty()) {
-      dev_.write((char*)&commandq_.front(), sizeof(recv_holder_t));
+      dev_.write((char *)&commandq_.front(), sizeof(recv_holder_t));
       commandq_.pop();
     }
     commandq_mutex_.unlock();
     std::this_thread::sleep_for(std::chrono::milliseconds(10));
   }
+
+  spdlog::debug("[Robot][ComThread] Stoped.");
 }
 
-void Robot::CommandThread() {}
+void Robot::CommandThread() {
+  spdlog::debug("[Robot][CommandThread] Running.");
+
+  spdlog::debug("[Robot][CommandThread] Stoped.");
+}
 
 Robot::Robot(const std::string &dev_path) {
-  std::cout << "Create Robot." << std::endl;
+  spdlog::debug("[Robot] Creating.");
 
   dev_.open(dev_path, std::ios::binary);
-  if (!dev_.is_open()) throw std::runtime_error("Can't open Robot device.");
+  if (!dev_.is_open()) {
+    spdlog::error("[Robot] Can't open Robot device.");
+    throw std::runtime_error("[Robot] Can't open Robot device.");
+  }
 
   continue_parse_ = true;
   parse_thread_ = std::thread(&Robot::ComThread, this);
+
+  spdlog::debug("[Robot] Created.");
 }
 
 Robot::~Robot() {
+  spdlog::debug("[Robot] Destroying.");
   dev_.close();
-  std::cout << "Camera Destried." << std::endl;
+  spdlog::debug("[Robot] Destried.");
 }
