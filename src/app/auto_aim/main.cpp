aa7db826ae57c6a580a4e65acbbac6c949d6aa39
Qu Shen <503578404@qq.com>
2020-08-23 20:55:41 +0800

    Add logging to file.

--- src/app/auto_aim/main.cpp
+++ src/app/auto_aim/main.cpp
@@ -3,6 +3,8 @@
 #include "behavior.hpp"
 #include "camera.hpp"
 #include "robot.hpp"
+#include "spdlog/sinks/basic_file_sink.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
 #include "spdlog/spdlog.h"
 #include "vision.hpp"
 
@@ -10,8 +12,19 @@ int main(int argc, char const* argv[]) {
   (void)argc;
   (void)argv;
 
+  auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+  auto file_sink =
+      std::make_shared<spdlog::sinks::basic_file_sink_mt>("logs/log.txt", true);
+
+  spdlog::sinks_init_list sink_list = {console_sink, file_sink};
+
+  spdlog::set_default_logger(
+      std::make_shared<spdlog::logger>("console & file", sink_list));
+
+  spdlog::flush_on(spdlog::level::debug);
   spdlog::set_level(spdlog::level::trace);
-  spdlog::warn("*****Running Auto Aim.*****");
+
+  spdlog::warn("***** Running Auto Aim. *****");
 
   Robot bot("/home/qs/virtual_robot");
   Camera cam(0);
