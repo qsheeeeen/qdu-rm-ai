5e7ee35fd7bbd07e21c33ce2c6ce30f05ce3eb63
Qu Shen <503578404@qq.com>
2020-08-23 16:15:31 +0800

    Fine tuned.

--- src/app/auto_aim/main.cpp
+++ src/app/auto_aim/main.cpp
@@ -10,13 +10,13 @@ int main(int argc, char const* argv[]) {
   (void)argc;
   (void)argv;
 
-  spdlog::set_level(spdlog::level::trace);  // Set specific logger's log level
-  spdlog::info("Running Auto Aim.");
+  spdlog::set_level(spdlog::level::trace);
+  spdlog::warn("*****Running Auto Aim.*****");
 
   Robot bot("/home/qs/virtual_robot.txt");
   Camera cam(0);
-  // Test only. TOOD: Remove.
 
+  // Test only. TOOD: Remove.
   TestTree();
   TestOpenCV();
   TestVideoWrite();
