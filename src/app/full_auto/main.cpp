ed0bd81e41f6ef9662df942cf0c3eb264532ad96
Qu Shen <503578404@qq.com>
2020-08-23 14:16:14 +0800

    Formated.

--- src/app/full_auto/main.cpp
+++ src/app/full_auto/main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 
-int main(int argc, char const *argv[])
-{
-    std::cout << "Fully automous started." << std::endl;
-    return 0;
+int main(int argc, char const *argv[]) {
+  std::cout << "Fully automous started." << std::endl;
+  return 0;
 }
--- src/camera/camera.hpp
+++ src/camera/camera.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <thread>
+
 #include "MvCameraControl.h"
 
 class Camera {
