d6c475f9fff61054870e518e14643485a199c977
Qu Shen <503578404@qq.com>
2020-08-26 16:44:27 +0800

    Add port to Node.

--- src/behavior/node.hpp
+++ src/behavior/node.hpp
@@ -7,20 +7,23 @@ namespace Action {
 
 class Track : public BT::SyncActionNode {
  public:
-  Track(const std::string &name);
+  Track(const std::string &name, const BT::NodeConfiguration& node_cfg);
   BT::NodeStatus tick() override;
+  static BT::PortsList providedPorts();
 };
 
 class Attack : public BT::SyncActionNode {
  public:
-  Attack(const std::string &name);
+  Attack(const std::string &name, const BT::NodeConfiguration& node_cfg);
   BT::NodeStatus tick() override;
+  static BT::PortsList providedPorts();
 };
 
 class Dodge : public BT::SyncActionNode {
  public:
-  Dodge(const std::string &name);
+  Dodge(const std::string &name, const BT::NodeConfiguration& node_cfg);
   BT::NodeStatus tick() override;
+  static BT::PortsList providedPorts();
 };
 
 }  // namespace Action
@@ -29,26 +32,30 @@ namespace Condition {
 
 class EnamyVisable : public BT::ConditionNode {
  public:
-  EnamyVisable(const std::string &name);
+  EnamyVisable(const std::string &name, const BT::NodeConfiguration& node_cfg);
   BT::NodeStatus tick() override;
+  static BT::PortsList providedPorts();
 };
 
 class LowHP : public BT::ConditionNode {
  public:
-  LowHP(const std::string &name);
+  LowHP(const std::string &name, const BT::NodeConfiguration& node_cfg);
   BT::NodeStatus tick() override;
+  static BT::PortsList providedPorts();
 };
 
 class UnderAttack : public BT::ConditionNode {
  public:
-  UnderAttack(const std::string &name);
+  UnderAttack(const std::string &name, const BT::NodeConfiguration& node_cfg);
   BT::NodeStatus tick() override;
+  static BT::PortsList providedPorts();
 };
 
 class NoAmmo : public BT::ConditionNode {
  public:
-  NoAmmo(const std::string &name);
+  NoAmmo(const std::string &name, const BT::NodeConfiguration& node_cfg);
   BT::NodeStatus tick() override;
+  static BT::PortsList providedPorts();
 };
 }  // namespace Condition
 
