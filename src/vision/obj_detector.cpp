c065af26ab0ec43dcb718df733210cb294b03f99
Qu Shen <503578404@qq.com>
2020-08-29 14:57:47 +0800

    Finish obj_detector

--- src/vision/obj_detector.cpp
+++ src/vision/obj_detector.cpp
@@ -1,267 +1,213 @@
 #include "obj_detector.hpp"
 
+#include <stdexcept>
+
+// #include "NvInfer.h"
+
 #define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_DEBUG
 #include "spdlog/spdlog.h"
 
-ObjectDetector::ObjectDetector() {}
-
-ObjectDetector::~ObjectDetector() {}
-
 #if 0
-bool ObjectDetector::build()
-{
-    auto builder = SampleUniquePtr<nvinfer1::IBuilder>(nvinfer1::createInferBuilder(sample::gLogger.getTRTLogger()));
-    if (!builder)
-    {
-        return false;
+using namespace nvinfer1;
+
+void TRTLogger::log(Severity severity, const char *msg) {
+  if (severity == kINTERNAL_ERROR) {
+    spdlog::error(msg);
+  } else if (severity == kERROR) {
+    spdlog::error(msg);
+  } else if (severity == kWARNING) {
+    spdlog::error(msg);
+  } else if (severity == kINFO) {
+    spdlog::error(msg);
+  } else if (severity == kVERBOSE) {
+    spdlog::error(msg);
+  }
+}
+
+bool ObjectDetector::ProcessInput(const BufferManager &buffers) {
+  const int in_height = dim_in.d[2];
+  const int in_width = dim_in.d[3];
+
+  // Read a random digit file
+  srand(unsigned(time(nullptr)));
+  std::vector<uint8_t> fileData(in_height * in_width);
+  mNumber = rand() % 10;
+  readPGMFile(locateFile(std::to_string(mNumber) + ".pgm", mParams.dataDirs),
+              fileData.data(), in_height, in_width);
+
+  float *hostDataBuffer =
+      static_cast<float *>(buffers.getHostBuffer(mParams.inputTensorNames[0]));
+  for (int i = 0; i < in_height * in_width; i++) {
+    hostDataBuffer[i] = 1.0 - float(fileData[i] / 255.0);
+  }
+
+  return true;
+}
+
+bool ObjectDetector::CreateEngine() {
+  auto builder = UniquePtr<IBuilder>(createInferBuilder(logger));
+  if (!builder) {
+    SPDLOG_ERROR("[ObjectDetector] createInferBuilder Fail.");
+    return false;
+  } else
+    SPDLOG_DEBUG("[ObjectDetector] createInferBuilder OK.");
+
+  const auto explicit_batch =
+      1U << static_cast<uint32_t>(
+          NetworkDefinitionCreationFlag::kEXPLICIT_BATCH);
+
+  auto network =
+      UniquePtr<INetworkDefinition>(builder->createNetworkV2(explicit_batch));
+  if (!network) {
+    SPDLOG_ERROR("[ObjectDetector] createNetworkV2 Fail.");
+    return false;
+  } else
+    SPDLOG_DEBUG("[ObjectDetector] createNetworkV2 OK.");
+
+  dim_in = network->getInput(0)->getDimensions();
+  dim_out = network->getOutput(0)->getDimensions();
+
+  SPDLOG_INFO("dim_out: {d}, dim_in: {d}.", dim_in.nbDims, dim_out.nbDims)
+
+  auto config = UniquePtr<IBuilderConfig>(builder->createBuilderConfig());
+  if (!config) {
+    SPDLOG_ERROR("[ObjectDetector] createBuilderConfig Fail.");
+    return false;
+  } else
+    SPDLOG_DEBUG("[ObjectDetector] createBuilderConfig OK.");
+
+  auto parser = UniquePtr<nvonnxparser::IParser>(
+      nvonnxparser::createParser(*network, logger);
+  if (!parser) {
+    SPDLOG_ERROR("[ObjectDetector] createParser Fail.");
+    return false;
+  }else 
+  SPDLOG_DEBUG("[ObjectDetector] createParser OK.");
+
+  auto parsed = parser->parseFromFile(
+      onnx_file_path.c_str(),
+      static_cast<int>(logger.getReportableSeverity()));
+  if (!parsed) {
+    SPDLOG_ERROR("[ObjectDetector] parseFromFile Fail.");
+    return false;
+  }else 
+  SPDLOG_DEBUG("[ObjectDetector] parseFromFile OK.");
+  
+  builder->setMaxBatchSize(1);
+  config->setMaxWorkspaceSize(1 << 30);
+
+  auto profile = builder->createOptimizationProfile();
+  profile->setDimensions(network->getInput(0)->getName(), OptProfileSelector::kMIN, Dims4{1, 3, 256 , 256});
+  profile->setDimensions(network->getInput(0)->getName(), OptProfileSelector::kOPT, Dims4{1, 3, 256 , 256});
+  profile->setDimensions(network->getInput(0)->getName(), OptProfileSelector::kMAX, Dims4{32, 3, 256 , 256});    
+  config->addOptimizationProfile(profile);
+  
+  if (use_fp16) config->setFlag(BuilderFlag::kFP16);
+
+  if (use_int8) {
+    config->setFlag(BuilderFlag::kINT8);
+    setAllTensorScales(network.get(), 127.0f, 127.0f);
+  }
+
+  if (use_dla_core >= 0) {
+    if (builder->getNbDLACores() == 0) {
+      SPDLOG_ERROR(
+          "Trying to use {d} DLA core on a platform that doesn't have any DLA "
+          "cores",
+          use_dla_core);
+      throw std::runtime_error(
+          "Error: use DLA core on a platfrom that doesn't have any DLA cores");
     }
 
-    const auto explicitBatch = 1U << static_cast<uint32_t>(NetworkDefinitionCreationFlag::kEXPLICIT_BATCH);
-    auto network = SampleUniquePtr<nvinfer1::INetworkDefinition>(builder->createNetworkV2(explicitBatch));
-    if (!network)
-    {
-        return false;
+    if (allow_gpu_fallback) {
+      config->setFlag(BuilderFlag::kGPU_FALLBACK);
     }
 
-    auto config = SampleUniquePtr<nvinfer1::IBuilderConfig>(builder->createBuilderConfig());
-    if (!config)
-    {
-        return false;
+    if (!builder->getInt8Mode() && !config->getFlag(BuilderFlag::kINT8)) {
+      // User has not requested INT8 Mode.
+      // By default run in FP16 mode. FP32 mode is not permitted.
+      builder->setFp16Mode(builder->platformHasFastFp16() && true);
+      config->setFlag(BuilderFlag::kFP16);
     }
+    config->setDefaultDeviceType(DeviceType::kDLA);
+    config->setDLACore(use_dla_core);
+    config->setFlag(BuilderFlag::kSTRICT_TYPES);
+  }
+  
+  SPDLOG_INFO("[ObjectDetector] CreateEngine, please wait for a while...");
 
-    auto parser = SampleUniquePtr<nvonnxparser::IParser>(nvonnxparser::createParser(*network, sample::gLogger.getTRTLogger()));
-    if (!parser)
-    {
-        return false;
-    }
+  engine = std::shared_ptr<ICudaEngine>(
+      builder->buildEngineWithConfig(*network, *config),
+      InferDeleter());
 
-    auto constructed = constructNetwork(builder, network, config, parser);
-    if (!constructed)
-    {
-        return false;
-    }
+  if (!engine) {
+    SPDLOG_ERROR("[ObjectDetector] CreateEngine Fail.");
+    return false;
+  }
+  SPDLOG_INFO("[ObjectDetector] CreateEngine OK.")
+  return true;
+}
 
-    mEngine = std::shared_ptr<nvinfer1::ICudaEngine>(
-        builder->buildEngineWithConfig(*network, *config), samplesCommon::InferDeleter());
-    if (!mEngine)
-    {
-        return false;
-    }
+bool ObjectDetector::LoadEngine() {
+  std::string buffer = readBuffer(engine_path);
+  if (buffer.size()) {
+    auto runtime = UniquePtr<IRuntime>(createInferRuntime(logger));
+    engine =
+        runtime->deserializeCudaEngine(buffer.data(), buffer.size(), nullptr);
+  }
+  if (!engine) {
+    SPDLOG_ERROR("[ObjectDetector] LoadEngine Fail.");
+    return false;
+  }
+  SPDLOG_DEBUG("[ObjectDetector] LoadEngine OK.");
+  return true;
+}
 
-    assert(network->getNbInputs() == 1);
-    mInputDims = network->getInput(0)->getDimensions();
-    assert(mInputDims.nbDims == 4);
-
-    assert(network->getNbOutputs() == 1);
-    mOutputDims = network->getOutput(0)->getDimensions();
-    assert(mOutputDims.nbDims == 2);
-
+bool ObjectDetector::SaveEngine() {
+  if (engine) {
+    auto engine_plan = UniquePtr<IHostMemory>(engine->serialize());
+    writeBuffer(engine_plan->data(), engine_plan->size(), engine_path);
+    SPDLOG_DEBUG("[ObjectDetector] SaveEngine OK.");
     return true;
+  }
+  SPDLOG_ERROR("[ObjectDetector] SaveEngine Fail.");
+  return false;
 }
 
-bool ObjectDetector::constructNetwork(
-    SampleUniquePtr<nvinfer1::IBuilder> &builder,
-    SampleUniquePtr<nvinfer1::INetworkDefinition> &network, 
-    SampleUniquePtr<nvinfer1::IBuilderConfig> &config,
-    SampleUniquePtr<nvonnxparser::IParser> &parser)
-{
-    auto parsed = parser->parseFromFile(locateFile(mParams.onnxFileName, mParams.dataDirs).c_str(),
-                                        static_cast<int>(sample::gLogger.getReportableSeverity()));
-    if (!parsed)
-    {
-        return false;
-    }
-
-    config->setMaxWorkspaceSize(16_MiB);
-    if (mParams.fp16)
-    {
-        config->setFlag(BuilderFlag::kFP16);
-    }
-    if (mParams.int8)
-    {
-        config->setFlag(BuilderFlag::kINT8);
-        samplesCommon::setAllTensorScales(network.get(), 127.0f, 127.0f);
-    }
-
-    samplesCommon::enableDLA(builder.get(), config.get(), mParams.dlaCore);
-
-    return true;
+bool ObjectDetector::CreateContex() {
+  auto context = UniquePtr<IExecutionContext>(engine->createExecutionContext());
+  if (!context) {
+    SPDLOG_ERROR("[ObjectDetector] CreateContex Fail.");
+    return false;
+  }
+  SPDLOG_DEBUG("[ObjectDetector] CreateContex OK.");
+  return true;
 }
 
-//!
-//! \brief Runs the TensorRT inference engine for this sample
-//!
-//! \details This function is the main execution function of the sample. It allocates the buffer,
-//!          sets inputs and executes the engine.
-//!
-bool ObjectDetector::infer()
-{
-    // Create RAII buffer manager object
-    samplesCommon::BufferManager buffers(mEngine);
-
-    auto context = SampleUniquePtr<nvinfer1::IExecutionContext>(mEngine->createExecutionContext());
-    if (!context)
-    {
-        return false;
-    }
-
-    // Read the input data into the managed buffers
-    assert(mParams.inputTensorNames.size() == 1);
-    if (!processInput(buffers))
-    {
-        return false;
-    }
-
-    // Memcpy from host input buffers to device input buffers
-    buffers.copyInputToDevice();
-
-    bool status = context->executeV2(buffers.getDeviceBindings().data());
-    if (!status)
-    {
-        return false;
-    }
-
-    // Memcpy from device output buffers to host output buffers
-    buffers.copyOutputToHost();
-
-    // Verify results
-    if (!verifyOutput(buffers))
-    {
-        return false;
-    }
-
-    return true;
+ObjectDetector::ObjectDetector() {
+  SPDLOG_DEBUG("[ObjectDetector] Creating.");
+  engine_path = onnx_file_path + ".engine";
+  camera.Open(0);
+  if (!LoadEngine()) CreateEngine();
+  CreateContex();
+  SPDLOG_DEBUG("[ObjectDetector] Created.");
 }
 
-//!
-//! \brief Reads the input and stores the result in a managed buffer
-//!
-bool ObjectDetector::processInput(const samplesCommon::BufferManager &buffers)
-{
-    const int inputH = mInputDims.d[2];
-    const int inputW = mInputDims.d[3];
-
-    // Read a random digit file
-    srand(unsigned(time(nullptr)));
-    std::vector<uint8_t> fileData(inputH * inputW);
-    mNumber = rand() % 10;
-    readPGMFile(locateFile(std::to_string(mNumber) + ".pgm", mParams.dataDirs), fileData.data(), inputH, inputW);
-
-    // Print an ascii representation
-    sample::gLogInfo << "Input:" << std::endl;
-    for (int i = 0; i < inputH * inputW; i++)
-    {
-        sample::gLogInfo << (" .:-=+*#%@"[fileData[i] / 26]) << (((i + 1) % inputW) ? "" : "\n");
-    }
-    sample::gLogInfo << std::endl;
-
-    float *hostDataBuffer = static_cast<float *>(buffers.getHostBuffer(mParams.inputTensorNames[0]));
-    for (int i = 0; i < inputH * inputW; i++)
-    {
-        hostDataBuffer[i] = 1.0 - float(fileData[i] / 255.0);
-    }
-
-    return true;
+ObjectDetector::~ObjectDetector() {
+  SPDLOG_DEBUG("[Robot] Destroying.");
+  camera.Close();
+  SPDLOG_DEBUG("[Robot] Destried.");
 }
 
-//!
-//! \brief Classifies digits and verify result
-//!
-//! \return whether the classification output matches expectations
-//!
-bool ObjectDetector::verifyOutput(const samplesCommon::BufferManager &buffers)
-{
-    const int outputSize = mOutputDims.d[1];
-    float *output = static_cast<float *>(buffers.getHostBuffer(mParams.outputTensorNames[0]));
-    float val{0.0f};
-    int idx{0};
-
-    // Calculate Softmax
-    float sum{0.0f};
-    for (int i = 0; i < outputSize; i++)
-    {
-        output[i] = exp(output[i]);
-        sum += output[i];
-    }
-
-    sample::gLogInfo << "Output:" << std::endl;
-    for (int i = 0; i < outputSize; i++)
-    {
-        output[i] /= sum;
-        val = std::max(val, output[i]);
-        if (val == output[i])
-        {
-            idx = i;
-        }
-
-        sample::gLogInfo << " Prob " << i << "  " << std::fixed << std::setw(5) << std::setprecision(4) << output[i]
-                         << " "
-                         << "Class " << i << ": " << std::string(int(std::floor(output[i] * 10 + 0.5f)), '*')
-                         << std::endl;
-    }
-    sample::gLogInfo << std::endl;
-
-    return idx == mNumber && val > 0.9f;
+bool ObjectDetector::Infer() {
+  CHECK(cudaMemcpyAsync(buffers[inputIndex], input,
+                        batchSize * 3 * INPUT_H * INPUT_W * sizeof(float),
+                        cudaMemcpyHostToDevice, stream));
+  context.enqueue(batchSize, buffers, stream, nullptr);
+  CHECK(cudaMemcpyAsync(output, buffers[outputIndex],
+                        batchSize * OUTPUT_SIZE * sizeof(float),
+                        cudaMemcpyDeviceToHost, stream));
+  cudaStreamSynchronize(stream);
 }
 
-//!
-//! \brief Initializes members of the params struct using the command line args
-//!
-samplesCommon::OnnxSampleParams initializeSampleParams(const samplesCommon::Args &args)
-{
-    samplesCommon::OnnxSampleParams params;
-    if (args.dataDirs.empty()) //!< Use default directories if user hasn't provided directory paths
-    {
-        params.dataDirs.push_back("data/mnist/");
-        params.dataDirs.push_back("data/samples/mnist/");
-    }
-    else //!< Use the data directory provided by the user
-    {
-        params.dataDirs = args.dataDirs;
-    }
-    params.onnxFileName = "mnist.onnx";
-    params.inputTensorNames.push_back("Input3");
-    params.outputTensorNames.push_back("Plus214_Output_0");
-    params.dlaCore = args.useDLACore;
-    params.int8 = args.runInInt8;
-    params.fp16 = args.runInFp16;
-
-    return params;
-}
-
-int main(int argc, char **argv)
-{
-    samplesCommon::Args args;
-    bool argsOK = samplesCommon::parseArgs(args, argc, argv);
-    if (!argsOK)
-    {
-        sample::gLogError << "Invalid arguments" << std::endl;
-        printHelpInfo();
-        return EXIT_FAILURE;
-    }
-    if (args.help)
-    {
-        printHelpInfo();
-        return EXIT_SUCCESS;
-    }
-
-    auto sampleTest = sample::gLogger.defineTest(gSampleName, argc, argv);
-
-    sample::gLogger.reportTestStart(sampleTest);
-
-    ObjectDetector sample(initializeSampleParams(args));
-
-    sample::gLogInfo << "Building and running a GPU inference engine for Onnx MNIST" << std::endl;
-
-    if (!sample.build())
-    {
-        return sample::gLogger.reportFail(sampleTest);
-    }
-    if (!sample.infer())
-    {
-        return sample::gLogger.reportFail(sampleTest);
-    }
-
-    return sample::gLogger.reportPass(sampleTest);
-}
 #endif
