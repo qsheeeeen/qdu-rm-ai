6d8b262c8dc7fa44433dc78604776d78a97ea4a3
Qu Shen <503578404@qq.com>
2020-08-31 21:49:27 +0800

    Default using all CPU core when training.

--- utils/train_vision.sh
+++ utils/train_vision.sh
@@ -5,13 +5,26 @@ python3 roco2x.py
 
 cd ../third_party/yolov5 || exit
 
+core_count=$(nproc --all)
+echo "CPU: ${core_count} cores."
+
 # Traing model. batch-size shown for 10 GB devices.
-python3 train.py --batch-size 26 --data ../../utils/dataset.yaml --img-size 608 608 --cfg ../../utils/model.yaml --hyp ../../utils/hyp.yaml --weights yolov5m.pt
-#--cache-images
+python3 train.py \
+--batch-size 26 \
+--data ../../utils/dataset.yaml \
+--img-size 608 608 \
+--cfg ../../utils/model.yaml \
+--hyp ../../utils/hyp.yaml \
+--weights yolov5m.pt \
+--worker ${core_count} \
+--cache-images
 
 # Test model.
-python3 detect.py --source ../../image/ --output ../../image/result --weights runs/exp0/weights/best.pt --conf 0.4
-#--cache-images
+python3 detect.py \
+--source ../../image/ \
+--output ../../image/result \
+--weights runs/exp0/weights/best.pt \
+--conf 0.4
 
 # Export model to ONNX for inference.
 export PYTHONPATH="$PWD"
