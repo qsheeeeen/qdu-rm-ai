77115d736f5eedea3e3575bb233d696bb484fb98
Qu Shen <503578404@qq.com>
2020-09-01 12:08:30 +0800

    Camera now capture into cv::Mat.

--- src/device/camera.hpp
+++ src/device/camera.hpp
@@ -3,18 +3,20 @@
 #include <thread>
 
 #include "MvCameraControl.h"
+#include "opencv2/opencv.hpp"
 
 class Camera {
  private:
-  MV_CC_DEVICE_INFO *mv_dev_info_;
   MV_CC_DEVICE_INFO_LIST mv_dev_list_;
-  MVCC_INTVALUE init_val_;
   void *camera_handle_ = nullptr;
+
   bool continue_capture_ = false;
   std::thread capture_thread_;
 
+  cv::Mat image(608, 608, CV_32FC3);
+
   void WorkThread();
-  void PrintDeviceInfo();
+  void PrintDeviceInfo(MV_CC_DEVICE_INFO *mv_dev_info);
   void Prepare();
 
  public:
