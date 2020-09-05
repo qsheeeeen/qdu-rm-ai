43982e314e16dedbe83d90ad18cb8b7104a7bd17
Qu Shen <503578404@qq.com>
2020-09-05 22:17:55 +0800

    Directlt get RGB frome camera.

--- src/device/camera.cpp
+++ src/device/camera.cpp
@@ -5,21 +5,28 @@
 #include <string>
 #include <thread>
 
-#define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_DEBUG
+#include "opencv2/imgproc.hpp"
 
+// TMP
+#include "opencv2/opencv.hpp"
+
+#define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_DEBUG
 #include "spdlog/spdlog.h"
 
 void Camera::PrintDeviceInfo(MV_CC_DEVICE_INFO *mv_dev_info) {
   if (nullptr == mv_dev_info) {
-    SPDLOG_ERROR("[Camera] The Pointer of mv_dev_info is nullptr!\n");
+    SPDLOG_ERROR("[Camera] The Pointer of mv_dev_info is nullptr!");
     return;
   }
   if (mv_dev_info->nTLayerType == MV_USB_DEVICE) {
-    SPDLOG_INFO(
-        "[Camera] UserDefinedName: {} \nSerial Number: {}\nDevice Number: {}",
-        mv_dev_info->SpecialInfo.stUsb3VInfo.chUserDefinedName,
-        mv_dev_info->SpecialInfo.stUsb3VInfo.chSerialNumber,
-        mv_dev_info->SpecialInfo.stUsb3VInfo.nDeviceNumber);
+    SPDLOG_INFO("[Camera] UserDefinedName: {}.",
+                mv_dev_info->SpecialInfo.stUsb3VInfo.chUserDefinedName);
+
+    SPDLOG_INFO("[Camera] Serial Number: {}.",
+                mv_dev_info->SpecialInfo.stUsb3VInfo.chSerialNumber);
+
+    SPDLOG_INFO("[Camera] Device Number: {}.",
+                mv_dev_info->SpecialInfo.stUsb3VInfo.nDeviceNumber);
   } else {
     SPDLOG_WARN("[Camera] Not support.");
   }
@@ -40,7 +47,7 @@ void Camera::Prepare() {
 
   if (mv_dev_list_.nDeviceNum > 0) {
     for (unsigned int i = 0; i < mv_dev_list_.nDeviceNum; ++i) {
-      SPDLOG_INFO("[Camera] Device {d} slected.", i);
+      SPDLOG_INFO("[Camera] Device {} slected.", i);
       MV_CC_DEVICE_INFO *dev_info = mv_dev_list_.pDeviceInfo[i];
       if (dev_info == nullptr) {
         SPDLOG_ERROR("[Camera] Error Reading dev_info");
@@ -52,6 +59,12 @@ void Camera::Prepare() {
     SPDLOG_ERROR("[Camera] Find No Devices!");
     throw std::runtime_error("[Camera] Find No Devices!");
   }
+}
+
+Camera::Camera() {
+  SPDLOG_DEBUG("[Camera] Constructing.");
+  Prepare();
+  SPDLOG_DEBUG("[Camera] Constructed.");
 }
 
 Camera::Camera(unsigned int out_h, unsigned int out_w)
@@ -75,6 +88,11 @@ Camera::~Camera() {
   SPDLOG_DEBUG("[Camera] Destructed.");
 }
 
+void Camera::Setup(unsigned int out_h, unsigned int out_w) {
+  out_h_ = out_h;
+  out_w_ = out_w;
+}
+
 int Camera::Open(unsigned int index) {
   int err = MV_OK;
   std::string err_msg;
@@ -89,25 +107,65 @@ int Camera::Open(unsigned int index) {
 
   err = MV_CC_CreateHandle(&camera_handle_, mv_dev_list_.pDeviceInfo[index]);
   if (err != MV_OK) {
-    SPDLOG_ERROR("[Camera] CreateHandle fail! err: {}", err);
+    SPDLOG_ERROR("[Camera] CreateHandle fail! err: {}.", err);
     return err;
   }
 
   err = MV_CC_OpenDevice(camera_handle_);
   if (err != MV_OK) {
-    SPDLOG_ERROR("[Camera] OpenDevice fail! err: {}", err);
+    SPDLOG_ERROR("[Camera] OpenDevice fail! err: {}.", err);
     return err;
   }
 
   err = MV_CC_SetEnumValue(camera_handle_, "TriggerMode", 0);
   if (err != MV_OK) {
-    SPDLOG_ERROR("[Camera] SetTrigger fail! err: {}", err);
+    SPDLOG_ERROR("[Camera] SetTrigger fail! err: {}.", err);
+    return err;
+  }
+
+  err = MV_CC_SetEnumValue(camera_handle_, "PixelFormat",
+                           PixelType_Gvsp_RGB8_Packed);
+  if (err != MV_OK) {
+    SPDLOG_ERROR("[Camera] PixelFormat fail! err: {}.", err);
+    return err;
+  }
+
+  err = MV_CC_SetEnumValue(camera_handle_, "AcquisitionMode", 2);
+  if (err != MV_OK) {
+    SPDLOG_ERROR("[Camera] AcquisitionMode fail! err: {}.", err);
+    return err;
+  }
+
+  MVCC_FLOATVALUE frame_rate;
+  err = MV_CC_GetFloatValue(camera_handle_, "ResultingFrameRate", &frame_rate);
+  if (err != MV_OK) {
+    SPDLOG_ERROR("[Camera] ResultingFrameRate fail! err: {}.", err);
+    return err;
+  } else {
+    SPDLOG_INFO("[Camera] ResultingFrameRate: {}.", frame_rate.fCurValue);
+  }
+
+  err = MV_CC_SetEnumValue(camera_handle_, "ExposureAuto", 2);
+  if (err != MV_OK) {
+    SPDLOG_ERROR("[Camera] AcquisitionMode fail! err: {}.", err);
+    return err;
+  }
+
+  err = MV_CC_SetEnumValue(camera_handle_, "GammaSelector", 2);
+  if (err != MV_OK) {
+    SPDLOG_ERROR("[Camera] AcquisitionMode fail! err: {}.", err);
+    return err;
+  }
+
+  err = MV_CC_SetBoolValue(camera_handle_, "GammaEnable", true);
+  if (err != MV_OK) {
+    SPDLOG_ERROR("[Camera] AcquisitionMode fail! err: {}.", err);
     return err;
   }
 
   err = MV_CC_StartGrabbing(camera_handle_);
   if (err != MV_OK) {
-    SPDLOG_ERROR("[Camera] StartGrabbing fail! err: {}", err);
+    SPDLOG_ERROR("[Camera] StartGrabbing fail! err: {}.", err);
     return err;
   }
   return MV_OK;
@@ -123,55 +181,38 @@ cv::Mat Camera::GetFrame() {
     SPDLOG_ERROR("[Camera] Camera disconnected.");
     goto finally;
   }
+
   err = MV_CC_GetImageBuffer(camera_handle_, &frame_out, 10);
   if (err == MV_OK) {
-    size_t rgb_size =
-        frame_out.stFrameInfo.nWidth * frame_out.stFrameInfo.nHeight * 4 + 2048;
+    SPDLOG_DEBUG("[Camera] Get One Frame: Width:{}, Height:{}.",
+                 frame_out.stFrameInfo.nWidth, frame_out.stFrameInfo.nHeight);
 
-    std::vector<unsigned char> raw_rgb(rgb_size);
-
-    MV_CC_PIXEL_CONVERT_PARAM cvt_param{
-        .nWidth = frame_out.stFrameInfo.nWidth,
-        .nHeight = frame_out.stFrameInfo.nHeight,
-
-        .enSrcPixelType = frame_out.stFrameInfo.enPixelType,
-        .pSrcData = frame_out.pBufAddr,
-        .nSrcDataLen = frame_out.stFrameInfo.nFrameLen,
-
-        .enDstPixelType = PixelType_Gvsp_RGB8_Packed,
-        .pDstBuffer = raw_rgb.data(),
-        .nDstBufferSize = static_cast<unsigned int>(rgb_size),
-    };
-
-    SPDLOG_DEBUG(
-        "[Camera] Get One Frame: Width{d}, Height{d}, "
-        "nFrameNum{d}\n",
-        frame_out.stFrameInfo.nWidth, frame_out.stFrameInfo.nHeight,
-        frame_out.stFrameInfo.nFrameNum);
-
-    err = MV_CC_ConvertPixelType(camera_handle_, &cvt_param);
-    if (MV_OK != err) {
-      SPDLOG_ERROR("[Camera] ConvertPixelType fail! err:{x}\n", err);
-      goto finally;
-    }
+    SPDLOG_DEBUG("[Camera] FrameNum: {}.", frame_out.stFrameInfo.nFrameNum);
 
     cv::Mat raw(
         cv::Size(frame_out.stFrameInfo.nWidth, frame_out.stFrameInfo.nHeight),
-        CV_8UC3, raw_rgb.data());
+        CV_8UC3, frame_out.pBufAddr);
 
-    const int offset_h = (raw.rows - raw.cols) / 2;
-    const cv::Rect roi(offset_h, 0, raw.cols, raw.cols);
+    // TEST ONLY
+    cv::cvtColor(raw, raw, cv::COLOR_RGB2BGR);
+    cv::imwrite("./image/camera_raw.jpg", raw);
+
+    const int offset_w = (raw.cols - raw.rows) / 2;
+    const cv::Rect roi(offset_w, 0, raw.rows, raw.rows);
     cv::resize(raw(roi), image, cv::Size(out_h_, out_w_));
 
+    // TEST ONLY
+    cv::imwrite("./image/camera_output.jpg", image);
+
   } else {
-    SPDLOG_ERROR("[Camera] GetImageBuffer fail! err:{x}\n", err);
+    SPDLOG_ERROR("[Camera] GetImageBuffer fail! err:{}.", err);
   }
 
 finally:
   if (NULL != frame_out.pBufAddr) {
     err = MV_CC_FreeImageBuffer(camera_handle_, &frame_out);
     if (err != MV_OK) {
-      SPDLOG_ERROR("[Camera] FreeImageBuffer fail! err:{x}\n", err);
+      SPDLOG_ERROR("[Camera] FreeImageBuffer fail! err:{}.", err);
     }
   }
   return image;
@@ -185,19 +226,19 @@ int Camera::Close() {
 
   err = MV_CC_StopGrabbing(camera_handle_);
   if (err != MV_OK) {
-    SPDLOG_ERROR("[Camera] StopGrabbing fail! err:{x}", err);
+    SPDLOG_ERROR("[Camera] StopGrabbing fail! err:{}.", err);
     return err;
   }
 
   err = MV_CC_CloseDevice(camera_handle_);
   if (err != MV_OK) {
-    SPDLOG_ERROR("[Camera] ClosDevice fail! err:{x}", err);
+    SPDLOG_ERROR("[Camera] ClosDevice fail! err:{}.", err);
     return err;
   }
 
   err = MV_CC_DestroyHandle(camera_handle_);
   if (err != MV_OK) {
-    SPDLOG_ERROR("[Camera] DestroyHandle fail! err:{x}", err);
+    SPDLOG_ERROR("[Camera] DestroyHandle fail! err:{}.", err);
     return err;
   }
 
--- src/device/camera.hpp
+++ src/device/camera.hpp
@@ -1,9 +1,7 @@
 #pragma once
 
-#include <thread>
-
 #include "MvCameraControl.h"
-#include "opencv2/opencv.hpp"
+#include "opencv2/core/mat.hpp"
 
 class Camera {
  private:
@@ -16,10 +14,12 @@ class Camera {
   void Prepare();
 
  public:
+  Camera();
   Camera(unsigned int out_h, unsigned int out_w);
   Camera(unsigned int index, unsigned int out_h, unsigned int out_w);
   ~Camera();
 
+  void Setup(unsigned int out_h, unsigned int out_w);
   int Open(unsigned int index);
 
   cv::Mat GetFrame();
