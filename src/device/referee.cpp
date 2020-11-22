2db492251f408b4e00ddd8363355378647abbda0
Qu Shen <503578404@qq.com>
2020-11-22 14:04:03 +0800

    Added comment to referee

--- src/device/referee.cpp
+++ src/device/referee.cpp
@@ -8,12 +8,21 @@
 
 #include "spdlog/spdlog.h"
 
+/**
+ * @brief Construct a new Referee:: Referee object
+ * 
+ */
 Referee::Referee() {
   SPDLOG_DEBUG("[Referee] Constructing.");
   dev_ = -1;
   SPDLOG_DEBUG("[Referee] Constructed.");
 }
 
+/**
+ * @brief Construct a new Referee:: Referee object
+ * 
+ * @param dev_path path to serial port
+ */
 Referee::Referee(const std::string& dev_path) {
   SPDLOG_DEBUG("[Referee] Constructing.");
 
@@ -27,20 +36,45 @@ Referee::Referee(const std::string& dev_path) {
   SPDLOG_DEBUG("[Referee] Constructed.");
 }
 
+/**
+ * @brief Destroy the Referee:: Referee object
+ * 
+ */
 Referee::~Referee() {
   SPDLOG_DEBUG("[Referee] Destructing.");
   close(dev_);
   SPDLOG_DEBUG("[Referee] Destructed.");
 }
 
+/**
+ * @brief Open serial port
+ * 
+ * @param dev_path path to serial port
+ */
 void Referee::Open(const std::string& dev_path) {
   dev_ = open(dev_path.c_str(), O_RDWR);
 
   if (dev_ < 0) SPDLOG_ERROR("[Referee] Can't open Referee device.");
 }
 
+/**
+ * @brief Check if serial port opened
+ * 
+ * @return true Opened
+ * @return false Not opened
+ */
 bool Referee::IsOpen() { return (dev_ > 0); }
 
+/**
+ * @brief config the serial port
+ * 
+ * @param parity 
+ * @param stop_bit 
+ * @param flow_ctrl 
+ * @param br Baudrate
+ * @return true config successed
+ * @return false error accrued
+ */
 bool Referee::Config(bool parity, bool stop_bit, bool flow_ctrl, BaudRate br) {
   struct termios tty_cfg;
 
@@ -89,10 +123,29 @@ bool Referee::Config(bool parity, bool stop_bit, bool flow_ctrl, BaudRate br) {
   return true;
 }
 
+/**
+ * @brief Send data
+ * 
+ * @param buff pointer to buffer 
+ * @param len  buffer length
+ * @return ssize_t length of buffer sent
+ */
 ssize_t Referee::Trans(const void* buff, size_t len) {
   return write(dev_, buff, len);
 }
 
+/**
+ * @brief Receive data
+ * 
+ * @param buff pointer to buffer
+ * @param len buffer length
+ * @return ssize_t ssize_t length of buffer receive
+ */
 ssize_t Referee::Recv(void* buff, size_t len) { return read(dev_, buff, len); }
 
+/**
+ * @brief close serial port
+ * 
+ * @return int status code
+ */
 int Referee::Close() { return close(dev_); }
