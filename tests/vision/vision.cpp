d5882ce09134df4c079694ce501e30756810c22c
Qu Shen <503578404@qq.com>
2020-12-26 17:41:59 +0800

    FIx opencv test

--- tests/vision/vision.cpp
+++ tests/vision/vision.cpp
@@ -6,12 +6,12 @@
 TEST(TestVision, ExampleTest) { ASSERT_EQ(1, 1); }
 
 TEST(TestVision, OpencvBasicIo) {
-  cv::Mat img = imread("./image/test.jpg", cv::IMREAD_COLOR);
+  cv::Mat img = imread("../../../image/test.jpg", cv::IMREAD_COLOR);
   ASSERT_FALSE(img.empty()) << "Can not opening image.";
 
   cvtColor(img, img, cv::COLOR_BGR2GRAY);
-  cv::imwrite("./image/test_gray.jpg", img);
+  cv::imwrite("../../../image/test_gray.jpg", img);
 
-  cv::Mat gray = imread("./image/test_gray.jpg", cv::IMREAD_GRAYSCALE);
+  cv::Mat gray = imread("../../../image/test_gray.jpg", cv::IMREAD_GRAYSCALE);
   ASSERT_FALSE(gray.empty()) << "Can not opening image.";
 }
