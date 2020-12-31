13569a723d22f2347b809e89108e51c3f1d84a80
Qu Shen <503578404@qq.com>
2020-12-31 21:03:54 +0800

    Update camera.cpp

--- tests/device/camera.cpp
+++ tests/device/camera.cpp
@@ -5,6 +5,8 @@
 #include "gtest/gtest.h"
 #include "opencv2/opencv.hpp"
 
+const std::string img_path = "../../../image/test_capture.jpg";
+
 TEST(TestCamera, TestCapture) {
   Camera cam;
   ASSERT_TRUE(cam.Open(0) == 0) << "Can not open camera 0.";
@@ -12,11 +14,11 @@ TEST(TestCamera, TestCapture) {
   cv::Mat frame = cam.GetFrame();
   ASSERT_FALSE(frame.empty()) << "Can not get frame from camera.";
 
-  cv::imwrite("../../../image/test_capture.jpg", frame);
-  std::ifstream f("../../../image/test_capture.jpg");
+  cv::imwrite(img_path, frame);
+  std::ifstream f(img_path);
   ASSERT_TRUE(f.good()) << "Can not write frame to file.";
   f.close();
 
-  cv::Mat img = imread("../../../image/test_capture.jpg", cv::IMREAD_COLOR);
+  cv::Mat img = imread(img_path, cv::IMREAD_COLOR);
   ASSERT_FALSE(img.empty()) << "Can not opening image after wrote.";
 }
