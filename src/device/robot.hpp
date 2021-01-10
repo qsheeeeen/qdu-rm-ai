b7bc32390ab2100b53862e7e53ecd81e67e0f09f
Qu Shen <503578404@qq.com>
2021-01-11 01:28:25 +0800

    float -> double

--- src/device/robot.hpp
+++ src/device/robot.hpp
@@ -7,11 +7,11 @@
 #include "serial.hpp"
 
 typedef struct {
-  float holder;
+  double holder;
 } RecvHolder;
 
 typedef struct {
-  float holder;
+  double holder;
 } CommandHolder;
 
 class Robot {
