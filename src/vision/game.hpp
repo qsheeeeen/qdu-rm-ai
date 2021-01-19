274651ad64c88c0affbeb4b511cd72ca10ac22e6
Qu Shen <503578404@qq.com>
2021-01-19 21:50:30 +0800

    Implement game function in source file.

--- src/vision/game.hpp
+++ src/vision/game.hpp
@@ -21,40 +21,7 @@ enum class Model {
   kBASE,
 };
 
-std::string TeamToString(Team team) {
-  switch (team) {
-    case Team::kUNKNOWN:
-      return std::string("Unknown");
-    case Team::kDEAD:
-      return std::string("Dead");
-    case Team::kBLUE:
-      return std::string("Blue");
-    case Team::kRED:
-      return std::string("Red");
-    default:
-      return std::string("Unknown");
-  }
-}
-
-std::string ModelToString(Model model) {
-  switch (model) {
-    case Model::kUNKNOWN:
-      return std::string("Unknown");
-    case Model::kINFANTRY:
-      return std::string("Infantry");
-    case Model::kHERO:
-      return std::string("Hero");
-    case Model::kENGINEER:
-      return std::string("Engineer");
-    case Model::kDRONE:
-      return std::string("Drone");
-    case Model::kSENTRY:
-      return std::string("Sentry");
-    case Model::kBASE:
-      return std::string("Base");
-    default:
-      return std::string("Unknown");
-  }
-}
+std::string TeamToString(Team team);
+std::string ModelToString(Model model);
 
 }  // namespace game
