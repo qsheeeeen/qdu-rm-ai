ce1c3efcdad25f037f4017f25cba870f28d66e59
Qu Shen <503578404@qq.com>
2021-01-19 20:31:16 +0800

    range estimator Prototype.

--- src/vision/range_estimator.cpp
+++ src/vision/range_estimator.cpp
@@ -3,6 +3,75 @@
 #include "opencv2/opencv.hpp"
 #include "spdlog/spdlog.h"
 
-RangeEstimator::RangeEstimator(/* args */) {}
+namespace {
 
-RangeEstimator::~RangeEstimator() {}
+const double kSMALL_ARMOR_WIDTH = 135.;
+const double KBIG_ARMOR_WIDTH = 230.;
+const double kARMOR_HEIGHT = 120.74072829;
+const double kARMOR_DEPTH = 32.35238064;
+
+const std::vector<cv::Point3f> kCOORD_SMALL_ARMOR{
+    cv::Point3f(-kSMALL_ARMOR_WIDTH / 2., kARMOR_HEIGHT / 2, kARMOR_DEPTH),
+    cv::Point3f(kSMALL_ARMOR_WIDTH / 2., kARMOR_HEIGHT / 2, 0.),
+    cv::Point3f(kSMALL_ARMOR_WIDTH / 2., kARMOR_HEIGHT / 2, 0.),
+    cv::Point3f(-kSMALL_ARMOR_WIDTH / 2., kARMOR_HEIGHT / 2, kARMOR_DEPTH)};
+
+const std::vector<cv::Point3f> kCOORD_BIG_ARMOR{
+    cv::Point3f(-KBIG_ARMOR_WIDTH / 2., kARMOR_HEIGHT / 2, kARMOR_DEPTH),
+    cv::Point3f(KBIG_ARMOR_WIDTH / 2., kARMOR_HEIGHT / 2, 0.),
+    cv::Point3f(KBIG_ARMOR_WIDTH / 2., kARMOR_HEIGHT / 2, 0.),
+    cv::Point3f(-KBIG_ARMOR_WIDTH / 2., kARMOR_HEIGHT / 2, kARMOR_DEPTH)};
+
+}  // namespace
+
+void RangeEstimator::LoadCameraMat(std::string path) {
+  fs_.open(path, cv::FileStorage::READ | cv::FileStorage::FORMAT_JSON);
+
+  if (fs_.isOpened()) {
+    cam_mat_ = fs_[cam_model_]["cam_mat"].mat();
+    distor_coff_ = fs_[cam_model_]["distor_coff"].mat();
+    SPDLOG_DEBUG("Loaded cali data.");
+  } else {
+    SPDLOG_ERROR("Can not load cali data for '{}' in '{}'", cam_model_, path);
+  }
+  fs_.release();
+}
+
+void RangeEstimator::PnpEstimate(Armor& armor) {
+  cv::Mat rotation, translation;
+  if (armor.GetModel() == game::Model::kHERO ||
+      armor.GetModel() == game::Model::kINFANTRY) {
+    cv::solvePnP(kCOORD_BIG_ARMOR, armor.Vertices(), cam_mat_, distor_coff_,
+                 rotation, translation, false, cv::SOLVEPNP_IPPE);
+  } else {
+    cv::solvePnP(kCOORD_SMALL_ARMOR, armor.Vertices(), cam_mat_, distor_coff_,
+                 rotation, translation, false, cv::SOLVEPNP_IPPE);
+  }
+  rotations_.push_back(rotation);
+  translations_.push_back(translation);
+}
+
+double RangeEstimator::PinHoleEstimate(std::vector<cv::Point2f> target) {}
+
+RangeEstimator::RangeEstimator() { SPDLOG_DEBUG("Constructed."); }
+
+RangeEstimator::RangeEstimator(std::string cam_model) {
+  Init(cam_model);
+  SPDLOG_DEBUG("Constructed.");
+}
+
+RangeEstimator::~RangeEstimator() { SPDLOG_DEBUG("Destructed."); }
+
+void RangeEstimator::Init(std::string cam_model) { SPDLOG_DEBUG("Inited."); }
+
+double RangeEstimator::Estimate(Armor& armor, double bullet_speed) {}
+
+void RangeEstimator::VisualizeResult(cv::Mat& output, bool add_lable) {
+  std::vector<cv::Point3d> nose_end_point3D{cv::Point3d(0., 0., 500.0)};
+  std::vector<cv::Point2d> nose_end_point2D;
+
+  // for (const auto& rotation : rotations_) {
+  //   cv::projectPoints(nose_end_point3D, rotation, translation, cam_mat_,
+  //                     distor_coff_, nose_end_point2D);
+  // }
+}
--- src/vision/range_estimator.hpp
+++ src/vision/range_estimator.hpp
@@ -1,11 +1,31 @@
 #pragma once
 
+#include <string>
+
+#include "armor.hpp"
+#include "game.hpp"
+#include "opencv2/opencv.hpp"
+
 class RangeEstimator {
  private:
-  /* data */
+  cv::FileStorage fs_;
+  std::string cam_model_;
+  cv::Mat cam_mat_;
+  cv::Mat distor_coff_;
+
+  std::vector<cv::Mat> rotations_;
+  std::vector<cv::Mat> translations_;
+
+  void LoadCameraMat(std::string path);
+  void PnpEstimate(Armor& armor);
+  double PinHoleEstimate(std::vector<cv::Point2f> target);
+
  public:
-  RangeEstimator(/* args */);
+  RangeEstimator();
+  RangeEstimator(std::string cam_model);
   ~RangeEstimator();
+
+  void Init(std::string cam_model);
+  double Estimate(Armor& armor, double bullet_speed);
+  void VisualizeResult(cv::Mat& output, bool add_lable = true);
 };
-
-
