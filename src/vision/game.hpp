13fac22f506f023072e56a1d078871d56deaa252
Qu Shen <503578404@qq.com>
2021-01-19 21:30:15 +0800

    Add function for ptinting.

--- src/vision/game.hpp
+++ src/vision/game.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <string>
+
 namespace game {
 
 enum class Team {
@@ -18,5 +20,41 @@ enum class Model {
   kSENTRY,
   kBASE,
 };
+
+std::string TeamToString(Team team) {
+  switch (team) {
+    case Team::kUNKNOWN:
+      return std::string("Unknown");
+    case Team::kDEAD:
+      return std::string("Dead");
+    case Team::kBLUE:
+      return std::string("Blue");
+    case Team::kRED:
+      return std::string("Red");
+    default:
+      return std::string("Unknown");
+  }
+}
+
+std::string ModelToString(Model model) {
+  switch (model) {
+    case Model::kUNKNOWN:
+      return std::string("Unknown");
+    case Model::kINFANTRY:
+      return std::string("Infantry");
+    case Model::kHERO:
+      return std::string("Hero");
+    case Model::kENGINEER:
+      return std::string("Engineer");
+    case Model::kDRONE:
+      return std::string("Drone");
+    case Model::kSENTRY:
+      return std::string("Sentry");
+    case Model::kBASE:
+      return std::string("Base");
+    default:
+      return std::string("Unknown");
+  }
+}
 
 }  // namespace game
