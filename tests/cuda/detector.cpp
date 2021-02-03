35e02dc20f2a86b020f2f58522e65814b52d72d2
Qu Shen <503578404@qq.com>
2021-02-03 16:35:13 +0800

    Move cuda related code to "nn" file.

--- tests/cuda/detector.cpp
+++
@@ -1,5 +0,0 @@
-#include <iostream>
-
-#include "gtest/gtest.h"
-
-TEST(TestCuda, ExampleTest) { EXPECT_EQ(1, 1); }
---
+++ tests/nn/detector.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+#include "gtest/gtest.h"
+
+TEST(TestNN, ExampleTest) { EXPECT_EQ(1, 1); }
