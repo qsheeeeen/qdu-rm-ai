3f36646302b95476625274dcc2c1e6780d05ca59
Qu Shen <503578404@qq.com>
2021-02-18 12:43:37 +0800

    Remove tpp

--- src/vision/detector.hpp
+++ src/vision/detector.hpp
@@ -18,10 +18,15 @@ class Detector {
   virtual void InitDefaultParams(const std::string &path) = 0;
   virtual bool PrepareParams(const std::string &path) = 0;
 
-  void LoadParams(const std::string &path);
+  void LoadParams(const std::string &path) {
+    if (!PrepareParams(path)) {
+      InitDefaultParams(path);
+      PrepareParams(path);
+      SPDLOG_WARN("Can not find parasm file. Created and reloaded.");
+    }
+    SPDLOG_DEBUG("Params loaded.");
+  }
 
   virtual const std::vector<Target> &Detect(const cv::Mat &frame) = 0;
   virtual void VisualizeResult(const cv::Mat &output, int verbose = 1) = 0;
 };
-
-#include "detector.tpp"
--- src/vision/detector.tpp
+++
@@ -1,9 +0,0 @@
-template <typename Target, typename Param>
-void Detector <Target, Param>::LoadParams(const std::string &path) {
-	if (!PrepareParams(path)) {
-		InitDefaultParams(path);
-		PrepareParams(path);
-		SPDLOG_WARN("Can not find parasm file. Created and reloaded.");
-	}
-	SPDLOG_DEBUG("Params loaded.");
-}
