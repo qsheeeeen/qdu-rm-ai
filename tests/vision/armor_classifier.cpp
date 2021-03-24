828b169874f4e27baf0b9aaf5b85b8129da63123
Qu Shen <503578404@qq.com>
2021-03-24 15:49:41 +0800

    Test all file for armor_classifier

--- tests/vision/armor_classifier.cpp
+++ tests/vision/armor_classifier.cpp
@@ -4,18 +4,26 @@
 #include "common.hpp"
 #include "gtest/gtest.h"
 
-TEST(TestVision, TestArmorClassifier) {
-  ArmorClassifier armor_classifier(
-      "../../../runtime/armor_classifier.onnx",
-      "../../../runtime/armor_classifier_lable.json", cv::Size(28, 28));
-  cv::Mat f = cv::imread("../../../image/p6.png");
+ArmorClassifier armor_classifier("../../../runtime/armor_classifier.onnx",
+                                 "../../../runtime/armor_classifier_lable.json",
+                                 cv::Size(28, 28));
+
+game::Model Classify(const std::string& path) {
+  cv::Mat f = cv::imread("../../../image/p2.png");
   Armor armor(cv::RotatedRect(cv::Point2f(0, 0), cv::Point2f(f.cols, 0),
                               cv::Point2f(f.cols, f.rows)));
 
-  cv::imwrite("../../../image/test_face.jpg", armor.Face(f));
+  cv::imwrite("../../../image/test_face.png", armor.Face(f));
   cv::Mat nn_input;
   cv::resize(armor.Face(f), nn_input, cv::Size(28, 28));
-  cv::imwrite("../../../image/test_nn_input.jpg", nn_input);
+  cv::imwrite("../../../image/test_nn_input.png", nn_input);
   armor_classifier.ClassifyModel(armor, f);
-  ASSERT_EQ(armor.GetModel(), game::Model::kHERO);
+  return armor.GetModel();
+}
+
+TEST(TestVision, TestArmorClassifier) {
+  ASSERT_EQ(Classify("../../../image/p2.png"), game::Model::kINFANTRY);
+  ASSERT_EQ(Classify("../../../image/p3.png"), game::Model::kENGINEER);
+  ASSERT_EQ(Classify("../../../image/p4.png"), game::Model::kHERO);
+  ASSERT_EQ(Classify("../../../image/p6.png"), game::Model::kHERO);
 }
