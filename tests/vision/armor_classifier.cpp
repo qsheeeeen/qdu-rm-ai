6e824ab5c42346f980735f9d3edd05e1a7dc9918
Qu Shen <503578404@qq.com>
2021-03-25 13:26:05 +0800

    Fix bug in armor classifier tests.

--- tests/vision/armor_classifier.cpp
+++ tests/vision/armor_classifier.cpp
@@ -9,14 +9,9 @@ ArmorClassifier armor_classifier("../../../runtime/armor_classifier.onnx",
                                  cv::Size(28, 28));
 
 game::Model Classify(const std::string& path) {
-  cv::Mat f = cv::imread("../../../image/p2.png");
+  cv::Mat f = cv::imread(path);
   Armor armor(cv::RotatedRect(cv::Point2f(0, 0), cv::Point2f(f.cols, 0),
                               cv::Point2f(f.cols, f.rows)));
-
-  cv::imwrite("../../../image/test_face.png", armor.Face(f));
-  cv::Mat nn_input;
-  cv::resize(armor.Face(f), nn_input, cv::Size(28, 28));
-  cv::imwrite("../../../image/test_nn_input.png", nn_input);
   armor_classifier.ClassifyModel(armor, f);
   return armor.GetModel();
 }
@@ -26,4 +21,15 @@ TEST(TestVision, TestArmorClassifier) {
   ASSERT_EQ(Classify("../../../image/p3.png"), game::Model::kENGINEER);
   ASSERT_EQ(Classify("../../../image/p4.png"), game::Model::kHERO);
   ASSERT_EQ(Classify("../../../image/p6.png"), game::Model::kHERO);
+}
+
+TEST(TestVision, TestArmorClassifierInput) {
+  cv::Mat f = cv::imread("../../../image/p2.png");
+  Armor armor(cv::RotatedRect(cv::Point2f(0, 0), cv::Point2f(f.cols, 0),
+                              cv::Point2f(f.cols, f.rows)));
+
+  cv::imwrite("../../../image/test_face.png", armor.Face(f));
+  cv::Mat nn_input;
+  cv::resize(armor.Face(f), nn_input, cv::Size(28, 28));
+  cv::imwrite("../../../image/test_nn_input.png", nn_input);
 }
